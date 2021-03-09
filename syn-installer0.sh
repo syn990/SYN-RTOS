7b6c82feae264a8ee6aadace020459bd757487d3
Syntax990 <79884715+syn990@users.noreply.github.com>
2021-03-09 01:00:23 +0000

    Update syn-installer0.sh
    The disk will surely run out of space if the installer attempts to updates ALL of it's packages.
    Instead now it will now just update the repo package lists.

--- syn-installer0.sh
+++ syn-installer0.sh
@@ -19,7 +19,7 @@
     # Update live enviroment with latest packages from /etc/mirrorlist
         # Use reflector and download the best mirrorlist for the live enviroment
             # Tell pacstrap to download all required packages to new install at /mnt
-                pacman -Syu
+                pacman -Syy
                     reflector -c "GB" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
                         pacstrap /mnt alsa-utils base base-devel dhcpcd dnsmasq dosfstools fakeroot feh gcc git htop iwd kitty kwrite linux linux-firmware lshw lxqt-qtplugin lxrandr nano openbox pcmanfm-qt qutebrowser pulseaudio python-pyalsa ranger reflector rsync spectacle sshfs sudo tint2 vlc xf86-video-vesa xorg-bdftopcf xorg-docs xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-encodings xorg-font-util xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-server-xephyr xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-setxkbmap xorg-smproxy xorg-x11perf xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmodmap xorg-xpr xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwininfo xorg-xwud xterm zsh terminus-font engrampa
 
