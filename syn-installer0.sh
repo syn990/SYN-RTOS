c784f1beb38dfa81397cf4349050369b52284aa2
Mr W Hayward-Holland <will@challoner.local>
2021-07-09 14:23:09 +0100

    Implemented pacstrap catagories
    It is easier to see what packages are installed, these should be a modifible list and one single pacstrap to install the lists as variables.

--- syn-installer0.sh
+++ syn-installer0.sh
@@ -22,14 +22,34 @@
                 pacman -Syy
                     #reflector -c "GB" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
                     ### Reflector should not be included until SYN-RTOS ISO has been released or else if it's not preempted it will fail at this step.
-                    pacstrap /mnt alsa-utils base base-devel dhcpcd dnsmasq dosfstools fakeroot feh gcc git htop iwd kitty kwrite linux linux-firmware lshw lxqt-qtplugin lxrandr nano openbox pcmanfm-qt chromium pulseaudio python-pyalsa ranger reflector rsync spectacle sshfs sudo tint2 vlc xf86-video-vesa xorg-bdftopcf xorg-docs xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-encodings xorg-font-util xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-server-xephyr xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-setxkbmap xorg-smproxy xorg-x11perf xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmodmap xorg-xpr xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwininfo xorg-xwud xterm zsh engrampa
-                    pacstrap /mnt terminus-font ttf-bitstream-vera 
+                    
+					#Install base system with development packages
+					pacstrap /mnt base base-devel dosfstools fakeroot feh gcc git htop kitty  linux linux-firmware sudo 
+					
+					#Install System Daemons
+					pacstrap /mnt alsa-utils dhcpcd dnsmasq iwd pulseaudio python-pyalsa 
+					
+					#Install Xorg Utilties
+					pacstrap /mnt xf86-video-vesa xorg-bdftopcf xorg-docs xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-encodings xorg-font-util xorg-iceauth xorg-mkfontscale xorg-server xorg-server-common xorg-server-devel xorg-server-xephyr xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-setxkbmap xorg-smproxy xorg-x11perf xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmodmap xorg-xpr xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwininfo xorg-xwud xterm
+					
+					#Install system utilties
+					pacstrap /mnt lxqt-qtplugin openbox tint2  zsh 
+                    
+					#Install CLI applications
+					pacstrap /mnt lshw nano reflector rsync ranger sshfs 
+					
+					#Install GUI applications
+					pacstrap /mnt chromium engrampa kwrite lxrandr pcmanfm-qt spectacle vlc
+					
+					#Install fonts
+					pacstrap /mnt terminus-font ttf-bitstream-vera 
+					
 # Generate filesystem table with boot information in respect to UUID assignment 
     genfstab -U /mnt >> /mnt/etc/fstab
 
 # Handover to part 2
-    mkdir /mnt/root/
-        cp -r /root/SYNSTALL/syn-installer0.sh      /mnt/root/
-        cp -r /root/SYNSTALL/syn-installer1.sh      /mnt/root/
+   # mkdir /mnt/root/
+   #     cp -r /root/SYNSTALL/syn-installer0.sh      /mnt/root/
+   #     cp -r /root/SYNSTALL/syn-installer1.sh      /mnt/root/
    #    cp -r /root/SYNSTALL/somefile.sh            /mnt/root/
-            arch-chroot /mnt sh /root/syn-installer1.sh
+   #         arch-chroot /mnt sh /root/syn-installer1.sh
