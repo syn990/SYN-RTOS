f2947cc13fa78707701c709e7341984cc91418e1
syn990 <william@syntax990.com>
2022-11-01 04:22:43 +0000

    Added pacstrap ASCII artwork

--- console-archiso-base/airootfs/root/syn-stage0.sh
+++ console-archiso-base/airootfs/root/syn-stage0.sh
@@ -28,6 +28,23 @@
 # Encryption is a very real possibility to be baked in both the live and
 # persistent envrooment.
 
+echo  "____    __    ____  __  .______    __  .__   __.   _______ "
+echo  "\   \  /  \  /   / |  | |   _  \  |  | |  \ |  |  /  _____|"
+echo  " \   \/    \/   /  |  | |  |_)  | |  | |   \|  | |  |  __  "
+echo  "  \            /   |  | |   ___/  |  | |  .    | |  | |_ | "
+echo  "   \    /\    /    |  | |  |      |  | |  |\   | |  |__| | "
+echo  "    \__/  \__/     |__| | _|      |__| |__| \__|  \______| "
+echo  " ___________    ____  _______ .______     ____    ____ .___________. __    __   __  .__   __.   _______ "
+echo  "|   ____\   \  /   / |   ____||   _  \    \   \  /   / |           ||  |  |  | |  | |  \ |  |  /  _____|"
+echo  "|  |__   \   \/   /  |  |__   |  |_)  |    \   \/   /   ---|  |---- |  |__|  | |  | |   \|  | |  |  __  "
+echo  "|   __|   \      /   |   __|  |      /      \_    _/       |  |     |   __   | |  | |  .    | |  | |_ | "
+echo  "|  |____   \    /    |  |____ |  |\  \----.   |  |         |  |     |  |  |  | |  | |  |\   | |  |__| | "
+echo  "|_______|   \__/     |_______|| _|  ._____|   |__|         |__|     |__|  |__| |__| |__| \__|  \______| "
+                                                                                                        
+                                                           
+                                                                                            
+#If a lable can be defined it it will really help with the entries and loader.conf for bootctl
+#Perhaps an MBR setup can leverage this?
 
 parted --script /dev/sda mklabel gpt mkpart primary fat32 1Mib 200Mib set 1 boot on
     parted --script /dev/sda mkpart primary ext4 201Mib 100%
@@ -37,6 +54,15 @@ parted --script /dev/sda mklabel gpt mkpart primary fat32 1Mib 200Mib set 1 boot
             mkdir /mnt/boot/
             mount /dev/sda1 /mnt/boot
  
+ 
+ 
+echo "     _______.____    ____ .__   __.        .______     .___________.  ______        _______."
+echo "    /       |\   \  /   / |  \ |  |        |   _  \    |           | /  __  \      /       |"
+echo "   |   (----  \   \/   /  |   \|  |  ______|  |_)  |    ---|  |---- |  |  |  |    |   (---- "
+echo "    \   \      \_    _/   |  .    | |______|      /        |  |     |  |  |  |     \   \    "
+echo ".----)   |       |  |     |  |\   |        |  |\  \----.   |  |     |   --'  | .----)   |   "
+echo "|_______/        |__|     |__| \__|        | _|  ._____|   |__|      \______/  |_______/    "
+ 
     # Update live enviroment with latest packages from /etc/mirrorlist
         # Use reflector and download the best mirrorlist for the live enviroment
             # Tell pacstrap to download all required packages to new install at /mnt
@@ -44,31 +70,91 @@ parted --script /dev/sda mklabel gpt mkpart primary fat32 1Mib 200Mib set 1 boot
                 
                     echo "Updating Mirrorlist"
                     reflector -c "GB" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-                    
-					echo "Installing base system with development packages"
-                    			pacstrap /mnt base base-devel dosfstools fakeroot gcc git linux linux-firmware sudo reflector pacman-contrib
-      
-					echo "Installing System Daemons"
-                    			pacstrap /mnt alsa-utils dhcpcd dnsmasq hostapd iwd pulseaudio python-pyalsa xorg-server x11vnc xcompmgr xorg-xinit
-                    
-					echo "Installing System Utilties"
-					pacstrap /mnt lxqt-qtplugin openbox tint2 zsh rsync pavucontrol-qt grub-customizer obconf-qt archlinux-xdg-menu
-					
-                    			echo "Installing CLI applications"
-					pacstrap /mnt lshw nano reflector ranger sshfs wget htop brightnessctl hdparm lshw yt-dlp
-                
-					echo "Installing GUI applications"
-					pacstrap /mnt chromium engrampa kwrite lxrandr pcmanfm-qt spectacle vlc feh kitty spectacle kdenlive gimp audacity
 
-					echo "Installing SYN-Fonts"
-					pacstrap /mnt terminus-font ttf-bitstream-vera
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing base system with development packages"
+
+
+    pacstrap /mnt base base-devel dosfstools fakeroot gcc git linux linux-firmware sudo reflector pacman-contrib
+
                     
-                    			echo "Installing SYN-RTOS Virtualization & Build set:"
-                    			pacstrap /mnt git archiso git qemu-desktop edk2-ovmf libvirt virt-manager virt-viewer hexedit binwalk android-tools
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing System Daemons"
+
+
+    pacstrap /mnt alsa-utils dhcpcd dnsmasq hostapd iwd man pulseaudio python-pyalsa xorg-server x11vnc xcompmgr xorg-xinit
+
                     
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing System Utilties"
+
+
+    pacstrap /mnt lxqt-qtplugin openbox tint2 zsh rsync pavucontrol-qt grub-customizer obconf-qt archlinux-xdg-menu
+
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing CLI applications"
+
+
+    pacstrap /mnt lshw nano reflector ranger sshfs wget htop brightnessctl hdparm lshw yt-dlp
+
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing GUI applications"
+
+    pacstrap /mnt chromium engrampa kwrite lxrandr pcmanfm-qt spectacle vlc feh kitty spectacle kdenlive gimp audacity
+
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing SYN-Fonts"
     
+    pacstrap /mnt terminus-font ttf-bitstream-vera
+
+echo  " ___  _   ___ ___ _____ ___    _   ___ "
+echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
+echo  "|  _/ _ \ (__\__ \ | | |   / / _ \|  _/"
+echo  "|_|/_/ \_\___|___/ |_| |_|_\/_/ \_\_|  "
+echo "Installing SYN-RTOS Virtualization & Build set:"
+
+    pacstrap /mnt git archiso git qemu-desktop edk2-ovmf libvirt virt-manager virt-viewer hexedit binwalk android-tools
+                    
+
 # This is just a requirement for boot functionality to exist on a physical disks
 # Could this be improved rather than being as part of a shellscript
     
 # Generate filesystem table with boot information in respect to UUID assignment 
+echo  " _______  _______  __    _  _______  _______  _______  _______  _______ "
+echo  "|       ||       ||  |  | ||       ||       ||       ||   _   ||  _    |"
+echo  "|    ___||    ___||   |_| ||    ___||  _____||_     _||  |_|  || |_|   |"
+echo  "|   | __ |   |___ |       ||   |___ | |_____   |   |  |       ||       |"
+echo  "|   ||  ||    ___||  _    ||    ___||_____  |  |   |  |       ||  _   | "
+echo  "|   |_| ||   |___ | | |   ||   |     _____| |  |   |  |   _   || |_|   |"
+echo  "|_______||_______||_|  |__||___|    |_______|  |___|  |__| |__||_______|"
+
     genfstab -U /mnt >> /mnt/etc/fstab
+
+echo  "  ____   ___ _____ _____ ___ _     _____ ____  "
+echo  " |  _ \ / _ \_   _|  ___|_ _| |   | ____/ ___| "
+echo  " | | | | | | || | | |_   | || |   |  _| \___ \ "
+echo  " | |_| | |_| || | |  _|  | || |___| |___ ___) |"
+echo  " |____/ \___/ |_| |_|   |___|_____|_____|____/ "
+                                               
+#Move the dotfiles to skel
+    cp -Rv /root/Dotfiles/.* /mnt/etc/skel/
+        
+        
