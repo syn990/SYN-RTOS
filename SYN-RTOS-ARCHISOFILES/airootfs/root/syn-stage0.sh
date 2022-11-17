7396f669ca489b84e5d19e596a0147bc07a107ba
Syntax990 <79884715+syn990@users.noreply.github.com>
2022-11-17 15:13:41 +0000

    Cleaned up erros

--- SYN-RTOS-ARCHISOFILES/airootfs/root/syn-stage0.sh
+++ SYN-RTOS-ARCHISOFILES/airootfs/root/syn-stage0.sh
@@ -9,8 +9,8 @@
 # Encryption is a very real possibility to be baked in both the live and
 # persistent enviroment.
 
-loadkeys uk			# Setup the keyboard layout
-timedatectl set-ntp true	# Setup NTP so the time is up-to-date
+	loadkeys uk			# Setup the keyboard layout
+	timedatectl set-ntp true	# Setup NTP so the time is up-to-date
 
 #  Stale. Also really scary... This is a "parted" script that aggressivley erases
 #  the disks of target /dev/sda which is inaccurate and dangerous to assume.
@@ -32,13 +32,13 @@ echo "If you didn't read the source properly you've probably wiped all your prec
 # This currently wipes sda, without any prompt. We need it to create a device lable as this will help bootctl be more predictable						   
 # It's set to gpt so a different script is needed for MBR setups. 
 
-parted --script /dev/sda mklabel gpt mkpart primary fat32 1Mib 200Mib set 1 boot on
-parted --script /dev/sda mkpart primary ext4 201Mib 100%
-	mkfs.vfat -F 32 /dev/sda1
-        mkfs.ext4 -F /dev/sda2
-        	mount /dev/sda2 /mnt
-        	mkdir /mnt/boot/
-		mount /dev/sda1 /mnt/boot
+	parted --script /dev/sda mklabel gpt mkpart primary fat32 1Mib 200Mib set 1 boot on
+	parted --script /dev/sda mkpart primary ext4 201Mib 100%
+		mkfs.vfat -F 32 /dev/sda1
+        	mkfs.ext4 -F /dev/sda2
+        		mount /dev/sda2 /mnt
+        		mkdir /mnt/boot/
+			mount /dev/sda1 /mnt/boot
  
 echo "     _______.____    ____ .__   __.        .______     .___________.  ______        _______."
 echo "    /       |\   \  /   / |  \ |  |        |   _  \    |           | /  __  \      /       |"
@@ -51,15 +51,16 @@ echo "|_______/        |__|     |__| \__|        | _|  ._____|   |__|      \____
 # It's been done this way so you can see what packages are being installed so you can make sensible decisions about what you want on the result system.
 # Literally, all you need to do is ensure the package name is present and it's a valid package, and pacstrap will install it.
 
-	BASE990"base base-devel dosfstools fakeroot gcc linux linux-firmware pacman-contrib reflector sudo zsh" # Base system"
-	DAEMON990="alsa-utils archlinux-xdg-menu dhcpcd dnsmasq hostapd iwd man pulseaudio python-pyalsa" # System Daemons
-	UTIL990="lxrandr obconf-qt pavucontrol-qt tint2conf" # System Utilties
-	SYS990="openbox xcompmgr xorg-server xorg-xinit tint2" # System UI
-	CLI990="brightnessctl git hdparm htop lshw nano ranger reflector rsync sshfs wget yt-dlp"# CLI applications
-	GUI990="audacity chromium engrampa feh gimp kdenlive kitty kwrite obs-studio openra pcmanfm-qt spectacle vlc" # GUI applications
-	FONT990="terminus-font ttf-bitstream-vera" # Fonts
-	BUILD990="android-tools archiso binwalk edk2-ovmf git hexedit libvirt qemu-desktop virt-manager virt-viewer" # SYN-RTOS Virtualization & Build set
-	SYN-RTOS-990="$BASE990 $DAEMON990 $UTIL990 $CLI990 $GUI990 $FONT990 $BUILD990" # Mastar Variable
+	BASE990"base base-devel dosfstools fakeroot gcc linux linux-firmware pacman-contrib reflector sudo zsh" 	# Base system
+	DAEMON990="alsa-utils archlinux-xdg-menu dhcpcd dnsmasq hostapd iwd man pulseaudio python-pyalsa" 		# System Daemons
+	UTIL990="lxrandr obconf-qt pavucontrol-qt" 									# System Utilties
+	SYS990="openbox xcompmgr xorg-server xorg-xinit tint2" 								# System UI
+	CLI990="brightnessctl git hdparm htop lshw nano ranger reflector rsync sshfs wget yt-dlp"			# CLI applications
+	GUI990="audacity chromium engrampa feh gimp kdenlive kitty kwrite obs-studio openra pcmanfm-qt spectacle vlc" 	# GUI applications
+	FONT990="terminus-font ttf-bitstream-vera" 									# Fonts
+	BUILD990="android-tools archiso binwalk edk2-ovmf git hexedit libvirt qemu-desktop virt-manager virt-viewer"	# SYN-RTOS Virtualization & Build set
+	SYN-RTOS-990="$BASE990 $DAEMON990 $UTIL990 $SYS990 $CLI990 $GUI990 $FONT990 $BUILD990" 				# Mastar Variable
+	
 
 echo  " ___  _   ___ ___ _____ ___    _   ___ "
 echo  "| _ \/_\ / __/ __|_   _| _ \  /_\ | _ |"""
@@ -72,7 +73,6 @@ echo "Installing packages to the resulting system - this might take a while so g
 
 	pacstrap /mnt $SYN-RTOS-990 
     
-
 echo  " _______  _______  __    _  _______  _______  _______  _______  _______ "
 echo  "|       ||       ||  |  | ||       ||       ||       ||   _   ||  _    |"
 echo  "|    ___||    ___||   |_| ||    ___||  _____||_     _||  |_|  || |_|   |"
@@ -91,7 +91,13 @@ echo  " | |_| | |_| || | |  _|  | || |___| |___ ___) |"
 echo  " |____/ \___/ |_| |_|   |___|_____|_____|____/ "
 # Move the 1.root_filesystem_overlay materials and cheap boot method across to the result system
 
-	cp -Rv /root/SYN-RTOS-V3/1.root_filesystem_overlay/* /mnt/
+	ROOTFSOVERLAY990="/root/SYN-RTOS-V3/1.root_filesystem_overlay/*"
+	cp -Rv $ROOTFSOVERLAY990 /mnt/ && clear
+	echo "Filesystems made"
+	echo "Partitions mounted"
+	echo "Pacstrap completed"
+	echo "Fstab generated"
+	echo "cp -Rv ROOTFSOVERLAY990 /mnt/ was successful"
 	
 echo "     _______.____    ____ .__   __.        .______     .___________.  ______        _______."
 echo "    /       |\   \  /   / |  \ |  |        |   _  \    |           | /  __  \      /       |"
